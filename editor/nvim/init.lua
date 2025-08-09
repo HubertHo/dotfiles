e8b9e21270ede45f5a7ecb7a3bf3fe4f3e4c2706
Hubert Ho <ho.hubert3@gmail.com>
2025-08-08 20:32:15 -0400

    Replace vim-signify with gitsigns.nvim

--- editor/nvim/init.lua
+++ editor/nvim/init.lua
@@ -134,12 +134,44 @@ require("lazy").setup({
     {"tpope/vim-fugitive"},
     {"tpope/vim-obsession"},
     {
-        "mhinz/vim-signify",
+        "lewis6991/gitsigns.nvim",
         config = function()
-            vim.g.signify_sign_add = "++"
-            vim.g.signify_sign_delete="--"
-            vim.g.signify_sign_delete_first_line="--"
-            vim.g.signify_sign_change="=="
+            local gitsigns = require("gitsigns")
+            gitsigns.setup {
+                signs = {
+                    add          = { text = ' ┃' },
+                    change       = { text = ' ┃' },
+                    delete       = { text = ' _' },
+                    topdelete    = { text = ' ‾' },
+                    changedelete = { text = ' ~' },
+                    untracked    = { text = ' ┆' },
+                },
+                signs_staged_enable = false,
+                on_attach = function(bufnr)
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map('n', ']c', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({']c', bang = true})
+                        else
+                            gitsigns.nav_hunk('next')
+                        end
+                    end)
+
+                    map('n', '[c', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({'[c', bang = true})
+                        else
+                            gitsigns.nav_hunk('prev')
+                        end
+                    end)
+                end
+            }
         end
     },
     {
